31c325e8a1c4616321cf51fca752dff2ac9d8307
filippovskiy09 <arturfilippovskij@gmail.com>
2024-08-31 09:27:12 +0300

    add some search logic

--- src/components/HomeComponent.jsx
+++ src/components/HomeComponent.jsx
@@ -8,19 +8,27 @@ import { useRef, useState } from "react";
 const HomeComponent = () => {
 	const user = useSelector((state) => state.auth.user);
 	const [searchActive, setSearchActive] = useState(false);
+	const [searchInput, setSearchInput] = useState("");
 	const ref = useRef(null);
 
 	return (
-		<div className="flex flex-col mt-7 h-full">
+		<div className="flex flex-col mt-7 h-full transition-all">
 			<div className="px-5">
-				<section className="mb-6">
+				<section className={`mb-6 ${searchActive || searchInput ? "hidden" : ""}`}>
 					<p className="mb-1">Hi, {user?.email}</p>
 					<h2 className="text-2xl font-bold">What are you looking for today?</h2>
 				</section>
-				<Input type="search" placeholder="Search headphone"/>
+				<Input 
+					type="search" 
+					placeholder="Search headphone"
+					onFocus={() => setSearchActive(true)}
+          onBlur={() => setSearchActive(false)}
+					setValue={setSearchInput}
+					value={searchInput}
+				/>
 			</div>
 			<section className="flex flex-col flex-1 bg-[#F6F6F6] w-full mt-6 ml-0 rounded-t-3xl">
-				<div className="px-5 overflow-x-hidden">
+				<div className={`px-5 overflow-x-hidden ${searchActive  && "opacity-0"}`}>
 					<Filters/>
 					<Products/>
 				</div>
--- src/components/shared/Input.jsx
+++ src/components/shared/Input.jsx
@@ -1,5 +1,4 @@
-'use client';
-const Input = ({ type, placeholder, value, setValue }) => {
+const Input = ({ type, placeholder, value, setValue, onFocus, onBlur }) => {
   const inputType = type ? type : "text";
 	
   return (
@@ -7,8 +6,10 @@ const Input = ({ type, placeholder, value, setValue }) => {
       type={inputType}
       placeholder={placeholder}
 			value={value}
+			onFocus={onFocus}
+			onBlur={onBlur}
 			onChange={(e) => setValue(e.target.value)}
-      className="w-full h-11 pl-4 flex items-center text-black text-sm placeholder:text-[#BABABA] rounded-lg border border-[#BABABA] bg-white focus:border-black focus:text-black outline-[#0ACF83]"
+      className="w-full h-11 pl-4 flex items-center text-black text-sm placeholder:text-[#BABABA] rounded-lg border border-[#BABABA] bg-white focus:border-black focus:text-black outline-[#0ACF83] transition-all ease-linear"
     />
   );
 };
